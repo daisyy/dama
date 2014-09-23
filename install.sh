4dfff500c8bef9d9f5e40c708732cb8bfe5410de
Jackong <JackongC@gmail.com>
2014-09-23 17:53:34 +0800

    update docker script

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ curl -s https://getcomposer.org/installer | php
 php composer.phar install
 
 # docker
-docker run -t -i -d --name dama-php -v /www/dama:/usr/local/nginx/html/dama:ro -e LISTEN=0.0.0.0:9000 andrei821/docker-php5-fpm
-
+docker run -t -i -d --name dama-php -v /www/dama/php.ini:/etc/php5/fpm/php.ini -v /www/dama:/usr/local/nginx/html/dama:ro daisy-php
