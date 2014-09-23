51b8433c936a847e5dd7634756f0d897753a1ecd
Jackong <JackongC@gmail.com>
2014-09-23 16:14:53 +0800

    refactor log

--- src/common/LogWriter.php
+++ src/common/LogWriter.php
@@ -7,9 +7,6 @@
 
 namespace src\common;
 
-
-use Slim\Slim;
-
 class LogWriter {
     private $file = null;
     public function __construct() {
@@ -24,11 +21,6 @@ class LogWriter {
         if (!is_string($message)) {
             $message = json_encode($message);
         }
-        $req = Slim::getInstance()->request();
-        $path = $req->getResourceUri();
-        $userAgent = $req->getUserAgent();
-        $method = $req->getMethod();
-        $params = json_encode($req->params());
-        fwrite($this->file, "$message|$method|$path|$params|$userAgent");
+        fwrite($this->file, $message);
     }
 } 
